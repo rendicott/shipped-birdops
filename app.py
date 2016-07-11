3416be6307f687482bb7f1ef18154457d391c545
Lab User <labuser@cisco.local>
2016-07-11 15:33:59 -0700

    First commit modification to shipped code, will see if we can redeploy docker

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!!"
+@app.route('/')
+def home():
+    return render_template('home.html')
 
+@app.route('/about')
+def about():    
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
